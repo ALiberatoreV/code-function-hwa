b624e57f558b26f6fb850624889820bdb80ae4a2
ALiberatoreV <aliberatore@mail.valenciacollege.edu>
2020-10-09 17:52:52 -0400

    3

---
+++ FUN.js
@@ -0,0 +1,18 @@
+function firstLetterName(name = anthony ){
+    alert("The name" + name + "starts with the letter" + A);
+}
+
+function divisibleByTwo(num = 4);{
+        if(num % 2) true
+        }
+
+function largestNum(bigNum = [5, 10, 20])
+
+
+
+
+
+
+return largestNum()
+return divisibleByTwo() 
+return firstLetterName()

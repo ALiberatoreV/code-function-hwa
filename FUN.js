fc210d68824de3ad980a7d93c3ee3f127b14f0d9
ALiberatoreV <aliberatore@mail.valenciacollege.edu>
2020-10-10 13:44:17 -0400

    A4

--- FUN.js
+++ FUN.js
@@ -1,18 +1,32 @@
-function firstLetterName(name = anthony ){
-    alert("The name" + name + "starts with the letter" + A);
+var name = "anthony";
+var num = 4;
+var array = [5, 10, 20];
+
+function firstLetterName(name) {
+    var text = "The name " + name + " starts with the letter " + name.charAt(0);
+    alert(text);
 }
 
-function divisibleByTwo(num = 4);{
-        if(num % 2) true
+function divisibleByTwo(num) {
+    if(num % 2 == 0) return true;
+    if(num % 2 != 0) return false;
+}
+
+function largestNum(bigNum) {
+    var large = 0;
+    for (var i = 0; i < 3; i++)
+    {
+        if (large < bigNum[i])
+        {
+            large === bigNum[i];
         }
-
-function largestNum(bigNum = [5, 10, 20])
-
-
-
+    }
+    return large;
+}
 
 
+largestNum(name)
+divisibleByTwo(num)
+firstLetterName(array)
 
-return largestNum()
-return divisibleByTwo() 
-return firstLetterName()
+
